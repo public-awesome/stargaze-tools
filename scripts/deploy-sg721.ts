ec74d766cd326df6310352299c4376802b2e9c55
Shane Vitarana <shanev@gmail.com>
2022-02-05 20:17:22 -0500

    Read config file

--- scripts/deploy-sg721.ts
+++ scripts/deploy-sg721.ts
@@ -6,26 +6,36 @@
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { calculateFee, GasPrice } from "@cosmjs/stargate";
-import config from "config";
 import * as fs from "fs";
 
-const rpcEndpoint: string = config.get('chain.rpc');
+const rpcEndpoint: string = config.get("chain.rpc");
 
 // Example user from scripts/wasmd/README.md
 const alice = {
-  mnemonic: "enlist hip relief stomach skate base shallow young switch frequent cry park",
+  mnemonic:
+    "enlist hip relief stomach skate base shallow young switch frequent cry park",
   address0: "wasm14qemq0vw6y3gc3u3e0aty2e764u4gs5lndxgyk",
 };
 
 async function main(sg721WasmPath: string) {
   const gasPrice = GasPrice.fromString("0ustars");
-  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(alice.mnemonic, { prefix: "stars" });
-  const client = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, wallet);
+  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(alice.mnemonic, {
+    prefix: "stars",
+  });
+  const client = await SigningCosmWasmClient.connectWithSigner(
+    rpcEndpoint,
+    wallet
+  );
 
   // Upload contract
   const wasm = fs.readFileSync(sg721WasmPath);
   const uploadFee = calculateFee(1_500_000, gasPrice);
-  const uploadReceipt = await client.upload(alice.address0, wasm, uploadFee, "Upload sg721 contract");
+  const uploadReceipt = await client.upload(
+    alice.address0,
+    wasm,
+    uploadFee,
+    "Upload sg721 contract"
+  );
   console.info("Upload succeeded. Receipt:", uploadReceipt);
 }
 
--- scripts/init.ts
+++ scripts/init.ts
@@ -1,30 +1,23 @@
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { calculateFee, GasPrice } from "@cosmjs/stargate";
-const fs = require("fs");
-
-const rpcEndpoint = "https://rpc.devnet.publicawesome.dev:443/";
-
-const alice = {
-  mnemonic: "enlist hip relief stomach skate base shallow young switch frequent cry park",
-};
-
-const codeId = 4;
+import fs from "fs";
 
 async function main() {
+  const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
   const gasPrice = GasPrice.fromString("0stars");
-  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(alice.mnemonic, { prefix: "stars" });
-  const client = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, wallet);
-
-  const accounts = await wallet.getAccounts();
-  console.log(accounts);
-  // Instantiate
+  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+    config["mnemonic"],
+    {
+      prefix: "stars",
+    }
+  );
+  const client = await SigningCosmWasmClient.connectWithSigner(
+    config["rpcEndpoint"],
+    wallet
+  );
   const instantiateFee = calculateFee(500_000, gasPrice);
 
-  const configData = fs.readFileSync("config.json");
-  const config = JSON.parse(configData);
-
-  // This contract specific message is passed to the contract
   const collectionMsg = {
     name: config["name"],
     symbol: config["symbol"],
@@ -33,11 +26,11 @@ async function main() {
   console.log(collectionMsg);
 
   const { contractAddress } = await client.instantiate(
-    accounts[0].address,
+    config["creator"],
     config["contractCodeId"],
     collectionMsg,
     config["name"],
-    instantiateFee,
+    instantiateFee
   );
   console.info(`Contract instantiated at: `, contractAddress);
 }
