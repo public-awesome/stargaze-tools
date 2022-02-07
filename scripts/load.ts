539b28cf8c07236b90d60e07ef04d70206ab33bb
Shane Vitarana <shanev@gmail.com>
2022-02-06 19:27:54 -0500

    Update contract name in config

--- scripts/load.ts
+++ scripts/load.ts
@@ -4,5 +4,5 @@ import fs from "fs";
 const CONFIG = "./config.json";
 const config = JSON.parse(fs.readFileSync(CONFIG, "utf8"));
 const client = await CosmWasmClient.connect(config["rpcEndpoint"]);
-// const contract = await client.getContract(config["minter"]);
-// const res = await client.queryContractSmart(config["minter"], {});
+// const contract = await client.getContract(config["collectionContract"]);
+// const res = await client.queryContractSmart(config["collectionContract"], {});
