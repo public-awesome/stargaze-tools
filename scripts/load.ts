da98b54af34b79571efc818e691f89437d368147
Shane Vitarana <shanev@gmail.com>
2022-02-19 15:30:16 -0500

    Remove commented code

--- scripts/load.ts
+++ scripts/load.ts
@@ -2,5 +2,3 @@ import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 const config = require("./config");
 
 const client = await CosmWasmClient.connect(config.rpcEndpoint);
-// const contract = await client.getContract(config["collectionContract"]);
-// const res = await client.queryContractSmart(config["collectionContract"], {});
