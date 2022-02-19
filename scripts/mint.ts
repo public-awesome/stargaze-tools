8b4bbf02a48a0d809d1030f8bf58460492c7773e
Shane Vitarana <shanev@gmail.com>
2022-02-19 17:00:25 -0500

    Better mint output

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -1,114 +1,105 @@
-import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
-import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
-import { calculateFee, coins, GasPrice } from '@cosmjs/stargate';
-import { Bech32 } from '@cosmjs/encoding';
+import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { calculateFee, coins, GasPrice } from "@cosmjs/stargate";
+import { Bech32 } from "@cosmjs/encoding";
 
-const addrHelper = require('./addrHelper');
-const config = require('./config');
+const addrHelper = require("./addrHelper");
+const config = require("./config");
 
 async function mintSender() {
-  const gasPrice = GasPrice.fromString('0stars');
-  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
-    config.mnemonic,
-    {
-      prefix: 'stars',
-    },
-  );
+  const gasPrice = GasPrice.fromString("0stars");
+  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(config.mnemonic, {
+    prefix: "stars",
+  });
   const client = await SigningCosmWasmClient.connectWithSigner(
     config.rpcEndpoint,
-    wallet,
+    wallet
   );
   const executeFee = calculateFee(300_000, gasPrice);
+
+  const msg = { mint: {} };
+  console.log(msg);
+
   const result = await client.execute(
     config.account,
     config.minter,
-    { mint: {} },
+    msg,
     executeFee,
-    'mint to sender',
-    coins('100000000', 'ustars'),
+    "mint to sender",
+    coins("100000000", "ustars")
   );
-  const wasmEvent = result.logs[0].events.find(
-    (e) => e.type === 'wasm',
-  );
+  const wasmEvent = result.logs[0].events.find((e) => e.type === "wasm");
   console.info(
-    'The `wasm` event emitted by the contract execution:',
-    wasmEvent,
+    "The `wasm` event emitted by the contract execution:",
+    wasmEvent
   );
 }
 
 async function mintTo(recipient: string) {
-  if (!recipient.startsWith('stars')) {
+  if (!recipient.startsWith("stars")) {
     const { data } = Bech32.decode(recipient);
-    const starsAddr = Bech32.encode('stars', data);
+    const starsAddr = Bech32.encode("stars", data);
     recipient = starsAddr;
   }
-  console.log('Minting to: ', recipient);
+  console.log("Minting to: ", recipient);
 
-  const gasPrice = GasPrice.fromString('0stars');
-  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
-    config.mnemonic,
-    {
-      prefix: 'stars',
-    },
-  );
+  const gasPrice = GasPrice.fromString("0stars");
+  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(config.mnemonic, {
+    prefix: "stars",
+  });
   const client = await SigningCosmWasmClient.connectWithSigner(
     config.rpcEndpoint,
-    wallet,
+    wallet
   );
   const executeFee = calculateFee(300_000, gasPrice);
+
+  const msg = { mint_to: { recipient: recipient } };
+  console.log(msg);
+
   const result = await client.execute(
     config.account,
     config.minter,
-    { mint_to: { recipient: recipient } },
+    msg,
     executeFee,
-    'mint to',
-    coins('100000000', 'ustars'),
+    "mint to",
+    coins("100000000", "ustars")
   );
-  const wasmEvent = result.logs[0].events.find(
-    (e) => e.type === 'wasm',
-  );
+  const wasmEvent = result.logs[0].events.find((e) => e.type === "wasm");
   console.info(
-    'The `wasm` event emitted by the contract execution:',
-    wasmEvent,
+    "The `wasm` event emitted by the contract execution:",
+    wasmEvent
   );
 }
 
 async function mintFor(tokenId: string, recipient: string) {
   let stars_recipient = addrHelper.to_stars_addr(recipient);
-  console.log('Minting token ' + tokenId + ' for', stars_recipient);
+  console.log("Minting token " + tokenId + " for", stars_recipient);
 
-  const gasPrice = GasPrice.fromString('0stars');
-  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
-    config.mnemonic,
-    {
-      prefix: 'stars',
-    },
-  );
+  const gasPrice = GasPrice.fromString("0stars");
+  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(config.mnemonic, {
+    prefix: "stars",
+  });
   const client = await SigningCosmWasmClient.connectWithSigner(
     config.rpcEndpoint,
-    wallet,
+    wallet
   );
   const executeFee = calculateFee(300_000, gasPrice);
+
+  const msg = { mint_for: { token_id: Number(tokenId), recipient } };
+  console.log(msg);
 
   const result = await client.execute(
     config.account,
     config.minter,
-    {
-      mint_for: {
-        token_id: Number(tokenId),
-        recipient: stars_recipient,
-      },
-    },
+    msg,
     executeFee,
-    'mint for',
-    coins('100000000', 'ustars'),
+    "mint for",
+    coins("100000000", "ustars")
   );
-  const wasmEvent = result.logs[0].events.find(
-    (e) => e.type === 'wasm',
-  );
+  const wasmEvent = result.logs[0].events.find((e) => e.type === "wasm");
   console.info(
-    'The `wasm` event emitted by the contract execution:',
-    wasmEvent,
+    "The `wasm` event emitted by the contract execution:",
+    wasmEvent
   );
 }
 
@@ -116,10 +107,10 @@ const args = process.argv.slice(6);
 console.log(args);
 if (args.length == 0) {
   await mintSender();
-} else if (args.length == 2 && args[0] == '--to') {
+} else if (args.length == 2 && args[0] == "--to") {
   await mintTo(args[1]);
-} else if (args.length == 3 && args[0] == '--for') {
+} else if (args.length == 3 && args[0] == "--for") {
   await mintFor(args[1], args[2]);
 } else {
-  console.log('Invalid arguments');
+  console.log("Invalid arguments");
 }
