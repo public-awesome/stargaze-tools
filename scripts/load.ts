4c58bc9da061e3f2a51ec99a0987c144c483e320
Shane Vitarana <shanev@gmail.com>
2022-02-19 15:21:14 -0500

    Update load script

--- scripts/load.ts
+++ scripts/load.ts
@@ -1,8 +1,6 @@
 import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
-import fs from "fs";
-
 const config = require("./config");
 
-const client = await CosmWasmClient.connect(config["rpcEndpoint"]);
+const client = await CosmWasmClient.connect(config.rpcEndpoint);
 // const contract = await client.getContract(config["collectionContract"]);
 // const res = await client.queryContractSmart(config["collectionContract"], {});
