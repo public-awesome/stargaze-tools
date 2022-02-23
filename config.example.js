69860669a9301996effaed645cb313aef35c5574
Jake Hartnell <jake.hartnell@gmail.com>
2022-02-22 16:18:09 -0800

    Update default royalties
    50% is too high for a default

--- config.example.js
+++ config.example.js
@@ -38,7 +38,7 @@ module.exports = {
   // The address for royalites to go to (may be the same as creator)
   royaltyAddress: 'stars1...',
   // Royalty share: 1 = 100%, 0.1 = 10%
-  royaltyShare: '0.5',
+  royaltyShare: '0.1',
   // The date when the sale goes live
   // If whitelist is enabled, only whitelisted addresses will be able to purchase
   startTime: '25 Dec 2021 00:00:00 GMT',
