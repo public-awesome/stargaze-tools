f37ae6f8f3bb92fdf75d5682c631de9086d7619e
Adrian Mummey <amummey@gmail.com>
2022-02-25 18:16:37 -0300

    Fix script

--- src/validation.ts
+++ src/validation.ts
@@ -1,23 +1,28 @@
-export const checkFiles = (images: string[], metadata: string[]) => {
+export const checkFiles = (
+  unsortedImages: string[],
+  unsortedMetadata: string[]
+) => {
+  const images = unsortedImages.sort((a, b) => parseInt(a) - parseInt(b));
+  const metadata = unsortedMetadata.sort((a, b) => parseInt(a) - parseInt(b));
   // Check images length is equal to metadata length
   if (images.length !== metadata.length) {
-    throw Error("Images files must have matching number of metadata files");
+    throw Error('Images files must have matching number of metadata files');
   }
 
   function parseFileName(path: string | null): string {
     // Check file name is not null
     if (!path) {
-      throw Error("File cannot be null");
+      throw Error('File cannot be null');
     }
 
     // Extract fileName from path
     const fileName = path.match(
-      /([a-zA-Z0-9\s_\\.\-:])+(.png|.jpg|.gif|.json)$/i
+      /([a-zA-Z0-9\s_\\.\-:]+)(.png|.jpg|.gif|.json)$/i
     )![1];
 
     // Check that file name is an Integer
     if (isNaN(parseInt(fileName, 10))) {
-      throw Error("Filenames must be numbers");
+      throw Error('Filenames must be numbers');
     }
     return fileName;
   }
@@ -27,12 +32,12 @@ export const checkFiles = (images: string[], metadata: string[]) => {
     let image = parseFileName(images[i]);
     let json = parseFileName(metadata[i]);
     if (image !== json) {
-      throw Error("Images must have matching JSON files");
+      throw Error('Images must have matching JSON files');
     }
     if (i !== 0) {
       let previousImage = parseFileName(images[i - 1]);
       if (image < previousImage) {
-        throw Error("Images must be sequential");
+        throw Error('Images must be sequential');
       }
     }
   }
