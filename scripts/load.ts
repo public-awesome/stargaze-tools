28a61ee290699344c6c3679c399a555dac1bbad8
Shane Vitarana <shanev@gmail.com>
2022-02-27 23:06:43 -0500

    Add num tokens to load script

--- scripts/load.ts
+++ scripts/load.ts
@@ -1,4 +1,7 @@
-import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
-const config = require("./config");
+import { CosmWasmClient } from '@cosmjs/cosmwasm-stargate';
+const config = require('./config');
 
 const client = await CosmWasmClient.connect(config.rpcEndpoint);
+const sg721 = config.sg721;
+const numTokens = await client.queryContractSmart(sg721, { num_tokens: {} });
+console.log('num tokens: ' + numTokens.count);
