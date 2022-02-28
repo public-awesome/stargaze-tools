9bf04b4bd8085081bad2f39c6f70314bd9617263
Shane Vitarana <shanev@gmail.com>
2022-02-28 12:12:25 -0500

    Fixed typo

--- scripts/mint-batch.ts
+++ scripts/mint-batch.ts
@@ -1,13 +1,13 @@
-import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
-import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
-import { calculateFee, coins, GasPrice } from "@cosmjs/stargate";
+import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+import { calculateFee, coins, GasPrice } from '@cosmjs/stargate';
 
-const config = require("./config");
+const config = require('./config');
 
 async function main(numMints: string) {
-  const gasPrice = GasPrice.fromString("0stars");
+  const gasPrice = GasPrice.fromString('0ustars');
   const wallet = await DirectSecp256k1HdWallet.fromMnemonic(config.mnemonic, {
-    prefix: "stars",
+    prefix: 'stars',
   });
   const client = await SigningCosmWasmClient.connectWithSigner(
     config.rpcEndpoint,
@@ -19,12 +19,12 @@ async function main(numMints: string) {
     config.minter,
     { batch_mint: { num_mints: Number(numMints) } },
     executeFee,
-    "batch mint",
-    coins("100000000", "ustars")
+    'batch mint',
+    coins('100000000', 'ustars')
   );
-  const wasmEvent = result.logs[0].events.find((e) => e.type === "wasm");
+  const wasmEvent = result.logs[0].events.find((e) => e.type === 'wasm');
   console.info(
-    "The `wasm` event emitted by the contract execution:",
+    'The `wasm` event emitted by the contract execution:',
     wasmEvent
   );
 }
