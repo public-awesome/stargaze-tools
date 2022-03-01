0c0160481e1723b9fa0c858cc601bc2232514b6c
John Y <j@publicawesome.com>
2022-03-01 15:03:21 -0500

    change image language

--- config.example.js
+++ config.example.js
@@ -31,10 +31,12 @@ module.exports = {
   symbol: 'SYM',
   // Project description
   description: 'An awesome NFT series',
-  // Path to image to use as the main image for the collection. gif compatible
+  // Link to image to use as the main image for the collection.
+  // Either IPFS or valid http links allowed. Gif compatible.
   // (at least 500 x 500 pixels)
-  image: 'images/1.png',
-  // external_link is optional. gif compatible
+  image:
+    'ipfs://bafybeigi3bwpvyvsmnbj46ra4hyffcxdeaj6ntfk5jpic5mx27x6ih2qvq/images/1.png',
+  // External_link is optional. Gif compatible
   //   external_link:
   // 'https://c.tenor.com/o656qFKDzeUAAAAC/rick-astley-never-gonna-give-you-up.gif',
 
