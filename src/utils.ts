b893d71fea2552505fbbe5bd934313b8f02a5520
jhernandezb <contact@jhernandez.me>
2022-03-09 20:52:13 -0600

    Provide feedback for invalid addresses

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,18 @@
 import { toBech32, fromBech32 } from 'cosmwasm';
 
 export const toStars = (addr: string) => {
-  const { data } = fromBech32(addr);
-  const starsAddr = toBech32('stars', data);
-  // wallet address length 20, contract address length 32
-  if (![20, 32].includes(data.length)) {
-    throw new Error('Invalid address: ' + addr + ' ' + starsAddr);
+  try {
+    const { data } = fromBech32(addr);
+    const starsAddr = toBech32('stars', data);
+    // wallet address length 20, contract address length 32
+    if (![20, 32].includes(data.length)) {
+      throw new Error('Invalid address: ' + addr + ' ' + starsAddr);
+    }
+    addr = starsAddr;
+    return addr;
+  } catch (e) {
+    throw new Error('Invalid address: ' + addr + ',' + e);
   }
-  addr = starsAddr;
-
-  return addr;
 };
 
 export const isValidHttpUrl = (uri: string) => {
